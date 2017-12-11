f4862e229e290cda4ed0edf5c817f4cbe57b1afd
Guoqiang Chen <subchen@gmail.com>
2017-12-11 10:58:28 +0800

    Create cmd_exec.go

---
+++ cmd_exec.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+)
+
+func ExecCommandOutput(name string, arg ...string) (output string, err error) {
+	cmd := exec.Command(name, arg...)
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+func ExecCommandStdpipe(name string, arg ...string) error {
+	cmd := exec.Command(name, arg...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
