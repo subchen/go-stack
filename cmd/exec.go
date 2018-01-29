d3afc31ac1e8c11106cd3f7945eaf2ffdf1d8570
Guoqiang Chen <subchen@gmail.com>
2018-01-29 11:52:34 +0800

    Create exec.go

---
+++ cmd/exec.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func ExecOutput(name string, arg ...string) (output string, err error) {
+	cmd := exec.Command(name, arg...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+func ShellOutput(shell string) (output string, err error) {
+	return ExecOutput("sh", "-c", shell)
+}
+
+func SystemExec(name string, arg ...string) error {
+	cmd := exec.Command(name, arg...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
