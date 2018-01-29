1a00d441130eeabf1e7b81936f4948a10d40a950
Guoqiang Chen <subchen@gmail.com>
2018-01-29 12:28:45 +0800

    Create exec_unix.go

---
+++ cmd/exec_unix.go
@@ -0,0 +1,26 @@
+package exec
+
+import (
+	"os"
+	"os/exec
+)
+
+func getShell() string {
+	shell := "/bin/sh"
+	if other := os.Getenv("SHELL"); other != "" {
+		shell = other
+	}
+	return shell
+}
+
+func ShellOutput(scripts string) (output string, err error) {
+	return ExecOutput(getShell(), "-c", scripts)
+}
+
+func Shell(scripts string) error {
+	shell := "/bin/sh"
+	if other := os.Getenv("SHELL"); other != "" {
+		shell = other
+	}
+	return Exec(getShell(), "-c", scripts)
+}
