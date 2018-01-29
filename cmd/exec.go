f4afa68cdcb7f998b4990f63171bae209b5ea4f7
Guoqiang Chen <subchen@gmail.com>
2018-01-29 12:27:10 +0800

    Update exec.go

--- cmd/exec.go
+++ cmd/exec.go
@@ -15,18 +15,10 @@ func ExecOutput(name string, arg ...string) (output string, err error) {
 	return string(out), nil
 }
 
-func ShellOutput(shell string) (output string, err error) {
-	return ExecOutput("sh", "-c", shell)
-}
-
 func Exec(name string, arg ...string) error {
 	cmd := exec.Command(name, arg...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
-}
-
-func Shell(shell string) error {
-	return Exec("sh", "-c", shell)
 }
