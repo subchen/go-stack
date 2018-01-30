769b7260864027435b7a6164ee95db6062e21d97
Guoqiang Chen <subchen@gmail.com>
2018-01-30 17:57:55 +0800

    Update and rename iif.go to iif/iif.go

---
+++ iif/iif.go
@@ -0,0 +1,128 @@
+package iif
+
+func String(condition bool, trueValue, falseValue string) string {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func Int(condition bool, trueValue, falseValue int) int {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+func Int8(condition bool, trueValue, falseValue int8) int8 {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func Int16(condition bool, trueValue, falseValue int16) int16 {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func Int32(condition bool, trueValue, falseValue int32) int32 {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func Int64(condition bool, trueValue, falseValue int64) int64 {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func IIfUint(condition bool, trueValue, falseValue uint) uint {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func Uint8(condition bool, trueValue, falseValue uint8) uint8 {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func Uint16(condition bool, trueValue, falseValue uint16) uint16 {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func Uint32(condition bool, trueValue, falseValue uint32) uint32 {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func Uint64(condition bool, trueValue, falseValue uint64) uint64 {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func Float32(condition bool, trueValue, falseValue float32) float32 {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func Float64(condition bool, trueValue, falseValue float64) float64 {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func Rune(condition bool, trueValue, falseValue rune) rune {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func Uintptr(condition bool, trueValue, falseValue uintptr) uintptr {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func Interface(condition bool, trueValue, falseValue interface{}) interface{} {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
