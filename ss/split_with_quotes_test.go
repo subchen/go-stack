d8db670026ba24ca6dc3238e6d6165ff57c50057
Guoqiang Chen <subchen@gmail.com>
2018-02-02 17:57:33 +0800

    Update split_with_quotes_test.go

--- ss/split_with_quotes_test.go
+++ ss/split_with_quotes_test.go
@@ -10,19 +10,22 @@ func TestSplitWithQuotes(t *testing.T) {
 		str    string
 		sep    string
 		quotes string
+		keep   bool
 		want   []string
 	}{
-		{"", ".", "[]", nil},
-		{"a.b.c", ".", "[]", []string{"a", "b", "c"}},
-		{"a.[b].c", ".", "[]", []string{"a", "b", "c"}},
-		{"a.[b.c]", ".", "[]", []string{"a", "b.c"}},
-		{"a.[b.c].dd  ", ".", "[]", []string{"a", "b.c", "dd"}},
-		{` select * from   "my table" `, " ", `"`, []string{"select", "*", "from", "my table"}},
+		{"", ".", "[]", false, nil},
+		{"a.b.c", ".", "[]", false, []string{"a", "b", "c"}},
+		{"a.[b].c", ".", "[]", false, []string{"a", "b", "c"}},
+		{"a.[b].c", ".", "[]", true, []string{"a", "[b]", "c"}},
+		{"a.[b.c]", ".", "[]", false, []string{"a", "b.c"}},
+		{"a.[b.c]", ".", "[]", true, []string{"a", "[b.c]"}},
+		{"a.[b.c].dd  ", ".", "[]", false, []string{"a", "b.c", "dd"}},
+		{` select * from   "my table" `, " ", `"`, false, []string{"select", "*", "from", "my table"}},
 	}
 
 	for _, test := range tests {
-		if got := SplitWithQuotes(test.str, test.sep, test.quotes); !reflect.DeepEqual(got, test.want) {
-			t.Errorf("SplitWithQuotes(%q, %q, %q) = %q; want: %q", test.str, test.sep, test.quotes, got, test.want)
+		if got := SplitWithQuotes(test.str, test.sep, test.quotes, test.keep); !reflect.DeepEqual(got, test.want) {
+			t.Errorf("SplitWithQuotes(%q, %q, %q, %q) = %q; want: %q", test.str, test.sep, test.quotes, test.keep, got, test.want)
 		}
 	}
 }
