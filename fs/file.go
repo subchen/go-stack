b9ff548a711c9371a609f3cf88834c586a9bc721
Guoqiang Chen <subchen@gmail.com>
2018-02-04 23:05:28 +0800

    Create file.go

---
+++ fs/file.go
@@ -0,0 +1,38 @@
+package fs
+
+import (
+  "os"
+)
+
+
+func fileSize(file string) int64 {
+	info, err := os.Stat(file)
+	if err != nil {
+		return -1
+	}
+	return info.Size()
+}
+
+func fileLastModified(file string) time.Time {
+	info, err := os.Stat(file)
+	if err != nil {
+		return time.Unix(0, 0)
+	}
+	return info.ModTime()
+}
+
+func fileGetBytes(file string) []byte {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil
+	}
+	return data
+}
+
+func fileGetString(file string) string {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return ""
+	}
+	return string(data)
+}
