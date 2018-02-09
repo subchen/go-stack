d22638fb36cf3573ad7536a1ecf8a2b4faa18d01
Guoqiang Chen <subchen@gmail.com>
2018-02-09 13:09:06 +0800

    Create blank.go

---
+++ ss/blank.go
@@ -0,0 +1,42 @@
+package ss
+
+import (
+	"bytes"
+	"unicode"
+)
+
+
+// DeleteWhitespaces deletes all whitespaces (unicode.IsSpace) in string.
+func DeleteWhitespaces(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	var changes bool
+	var buf bytes.Buffer
+	for _, r := range s {
+		if unicode.IsSpace(r) {
+			changes = true
+		} else {
+			buf.WriteRune(r)
+		}
+	}
+
+	if !changes {
+		return s
+	}
+	return buf.String()	
+}
+
+// IsBlank returns whether a string is whitespace or empty.
+func IsBlank(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	for _, r := range s {
+		if unicode.IsSpace(r) == false {
+			return false
+		}
+	}
+	return true
+}
