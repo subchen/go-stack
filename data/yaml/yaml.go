2fbc049db581eb6efc33b62fef0d64b6a13102b0
Guoqiang Chen <subchen@gmail.com>
2018-02-28 16:21:53 +0800

    Update yaml.go

--- data/yaml/yaml.go
+++ data/yaml/yaml.go
@@ -1,9 +1,10 @@
 package yaml
 
 import (
+	"io/ioutil"
+
 	"github.com/go-yaml/yaml"
 	"github.com/subchen/go-stack/data"
-	"github.com/subchen/go-stack/fs"
 )
 
 func NewQuery(data []byte) (*data.Query, error) {
@@ -19,7 +20,7 @@ func NewStringQuery(data string) (*data.Query, error) {
 }
 
 func NewFileQuery(filename string) (*data.Query, error) {
-	data, err := fs.FileGetBytes(filename)
+	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
