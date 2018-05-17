1e117165ad6dd9b439c36718f0ce4c8bfc1e9374
Guoqiang Chen <subchen@gmail.com>
2018-05-17 14:26:36 +0800

    Create json.go

---
+++ encoding/json/json.go
@@ -0,0 +1,61 @@
+package json
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+)
+
+// Marshal returns the JSON encoding of v
+func Marshal(v interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	encoder := json.NewEncoder(&buf)
+	encoder.SetEscapeHTML(false)
+	err := encoder.Encode(v)
+	if err != nil {
+		return nil, err
+	}
+
+	// trim last "\n"
+	b := buf.Bytes()
+	if n := len(b); n > 0 && b[n-1] == '\n' {
+		b = b[:n-1]
+	}
+	return b, nil
+}
+
+// MarshalIndent is like Marshal but applies Indent to format the output
+func MarshalIndent(v interface{}, prefix, indent string) ([]byte, error) {
+	var buf bytes.Buffer
+	encoder := json.NewEncoder(&buf)
+	encoder.SetEscapeHTML(false)
+	encoder.SetIndent(prefix, indent)
+	err := encoder.Encode(v)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+// Unmarshal parses the JSON-encoded data and stores the result in the value pointed to by v.
+func Unmarshal(data []byte, v interface{}) error {
+	return json.Unmarshal(data, v)
+}
+
+// ReadFile reads json from file and unmarshal into result
+func ReadFile(filename string, result interface{}) error {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	return Unmarshal(data, result)
+}
+
+// WriteFile marshals data and writes to file
+func WriteFile(filename string, data interface{}) error {
+	bytes, err := MarshalIndent(data, "", "  ")
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filename, bytes, 0755)
+}
